c71f6ea2778b65522584f71ffc95f6ae79e4db4b
Quang Minh <0947556012abc@gmail.com>
2026-07-04 01:06:56 +0700

    chore: remove unused components and test files

--- packages/frontend/src/components/landing/sections/index.ts
+++ packages/frontend/src/components/landing/sections/index.ts
@@ -1,6 +1,5 @@
 export { HeroSection } from "./HeroSection";
 export { QuickstartSection } from "./QuickstartSection";
-export { WorldwideSection } from "./WorldwideSection";
 export { DescriptionSection } from "./DescriptionSection";
 export { FollowSection } from "./FollowSection";
 export { FooterSection } from "./FooterSection";
